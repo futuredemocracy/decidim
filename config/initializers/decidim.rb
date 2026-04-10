f25f1306f58d9ea21346ded9238d72aa336a4bfb
raderr <rader1995@gmail.com>
2026-04-10 12:54:09 +0200

    Add czech locale

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -1,5 +1,5 @@
 Decidim.configure do |config|
-  config.available_locales = %i[en pl]
+  config.available_locales = %i[en pl cz]
   config.default_locale = :pl
   config.sms_gateway_service = "Decidim::Verifications::Sms::Gateway"
 end
